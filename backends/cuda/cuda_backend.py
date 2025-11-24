a7c07f561d0f8b74e4c1bb7e18ef587e00fbf273
Jacob Szwejbka <jakeszwe@fb.com>
2025-11-24 15:30:38 -0800

    missing import

--- backends/cuda/cuda_backend.py
+++ backends/cuda/cuda_backend.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 import typing
 from importlib import resources
 from typing import Any, Dict, final, List
